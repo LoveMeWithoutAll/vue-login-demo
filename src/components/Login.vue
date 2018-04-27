3ecd3e1cad43e6155bc55ff20b6e954a67fd3598
YoungSeon.Ahn <ys.ahn@outlook.com>
2018-04-27 22:29:13 +0900

    Use vee-validate

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,10 +4,11 @@
       <div class="alert-danger" v-if="errorState">
         <p>{{ errorState }}</p>
       </div>
-      <form @submit="onSubmit">
-          <input placeholder="Enter your ID" v-model="uid">
-          <input placeholder="Enter your password" v-model="password">
+      <form @submit.prevent="onSubmit">
+          <input name="uid" placeholder="Enter your ID" v-model="uid" v-validate="'required'">
+          <input name="password" placeholder="Enter your password" v-model="password" type="password" v-validate="'required|min:6'">
           <button type="submit">Login</button>
+          <div class="alert-danger" v-if="errors.has('password')">{{errors.first('password')}}</div>
       </form>
   </div>
 </template>
@@ -24,11 +25,16 @@ export default {
   methods: {
     ...mapActions(['login']),
     async onSubmit () {
-      try {
-        let loginResult = await this.login({uid: this.uid, password: this.password})
-        if (loginResult) this.goToPages()
-      } catch (err) {
-        console.error(err)
+      this.$validator.validateAll()
+      if (!this.errors.any()) {
+        try {
+          let loginResult = await this.login({uid: this.uid, password: this.password})
+          if (loginResult) this.goToPages()
+        } catch (err) {
+          console.error(err)
+        }
+      } else {
+        console.log('validate err')
       }
     },
     goToPages () {
@@ -46,7 +52,7 @@ export default {
 </script>
 
 <style scoped>
-.alert-danger p{
+.alert-danger {
   color: red;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './vuex/store'
+import VeeValidate from 'vee-validate'
 
+Vue.use(VeeValidate)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
